731d5ef4d57c9e1aa4ab619deeab86dd2b850776
LeoPotiers <65218137+LeoPotiers@users.noreply.github.com>
2020-07-08 10:04:56 +0200

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -28,6 +28,7 @@ def xml_to_csv(path):
 def main():
     for directory in ['Train','Test']:
         image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
+        #image_path = os.path.join(os.getcwd(), '/DepotML//{}'.format(directory))
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
